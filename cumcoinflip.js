ac410f33f0f245efb0b55e599b681e936954aa57
nuts <plopkoek54@gmail.com>
2021-02-07 22:03:13 +0100

    edit coinflip code

--- cumcoinflip.js
+++ cumcoinflip.js
@@ -2,7 +2,7 @@ function coinFlip() {
 function flip() {
     return Math.floor((Math.random() * 2) + 1)} 
     let result = flip(); 
-    if (result===1) {
+    if (result==1) {
         winner= 'HEADS'
         message.channel.send(`Je hebt **${winner}** gecummed`);
         } 
@@ -10,10 +10,10 @@ function flip() {
          verliezer = 'HEADS'
          message.channel.send(`Je hebt **${verliezer}** gecummed`); 
          }
-    { 
+    
      (message.content === `${prefix}poep`) 
         message.channel.send ();
-    } 
+    
 }
 
             
