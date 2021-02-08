c4981ae88fc2032d2285ce14bd29c618e9384f43
Bart <bartvanmoorsel7@gmail.com>
2021-02-08 01:00:18 +0100

    changed if-else to switch statement, added shutdown command

--- main.js
+++ main.js
@@ -6,36 +6,48 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-		
-client.on('message', message => { 
-	function flip() {
-	return Math.floor((Math.random() * 2) + 1)} 
-	result = flip(); 
-	
-	if (result===1) {
-		winner= 'HEADS'
-		} 
-	else {
-		 verliezer = 'Tails' }
 
-	if (message.content === `${prefix}cum`) {
-		message.channel.send('ik cum');
-	} else if (message.content === `${prefix}thomas`) {
-		message.channel.send('stinkt heel erg');
-	} else if (message.content === `${prefix}diamo`) {
-		message.channel.send('stinkt niet');
-	} else if (message.content === `${prefix}server`) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else if (message.content === `${prefix}poepinbroek`) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	} else if 
-			 (message.content === `${prefix}poep`) {
-			 message.channel.send (` ${result}   `);
-				
-			} 
-		}
-		
-	) 
+client.on('message', message => {
+	function flip() {
+		return Math.floor((Math.random() * 2) + 1)
+	}
+	result = flip();
+
+	if (result === 1) {
+		winner = 'HEADS'
+	}
+	else {
+		verliezer = 'Tails'
+	}
+
+	switch (message.content) {
+		case `${prefix}cum`:
+			message.channel.send('ik cum');
+			break;
+		case `${prefix}thomas`:
+			message.channel.send('stinkt heel erg');
+			break;
+		case `${prefix}diamo`:
+			message.channel.send('stinkt niet');
+			break;
+		case `${prefix}server`:
+			message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+			break;
+		case `${prefix}poepinbroek`:
+			message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+			break;
+		case `${prefix}poep`:
+			message.channel.send(` ${result}   `);
+			break;
+		case `${prefix}shutdown`:
+			message.channel.send(`Shutting down `);
+			resetBot(message.channel);
+			break;
+		default:
+			break;
+	}
+}
+)
 
 
 client.login(token);
